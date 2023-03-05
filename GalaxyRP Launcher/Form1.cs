2e433fbbdd43ebfcc973ccb4b8711297c945aaf5
Alex Nita <67995057+alexnita3@users.noreply.github.com>
2023-03-05 01:18:09 +0000

    Added necessary nuget packages

--- GalaxyRP Launcher/Form1.cs
+++ GalaxyRP Launcher/Form1.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
 namespace GalaxyRP_Launcher
 {
     public partial class Form1 : Form
