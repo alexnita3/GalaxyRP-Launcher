9a72ef7bdd9d468a24752e4671885b9f1603699e
Alex Nita <67995057+alexnita3@users.noreply.github.com>
2023-03-05 01:35:03 +0000

    Oauth works

--- GalaxyRP Launcher/Form1.cs
+++ GalaxyRP Launcher/Form1.cs
@@ -7,8 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
 using Google.Apis.Drive.v3.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
@@ -24,7 +28,29 @@ namespace GalaxyRP_Launcher
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Test();
+        }
 
+        private async Task Test()
+        {
+            UserCredential credential;
+            using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read)) {
+
+                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    new[] { DriveService.Scope.DriveReadonly },
+                    "user", CancellationToken.None, new FileDataStore("Drive.ListMyFiles"));   
+            }
+
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "GalaxyRP Launcher",
+            });
+
+            var filelist = await service.Files.List().ExecuteAsync();
+
+            listBox1.Items.Add(filelist);
         }
     }
 }
