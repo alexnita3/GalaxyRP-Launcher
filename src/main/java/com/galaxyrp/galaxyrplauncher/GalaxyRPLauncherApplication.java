e1f8859c650521f4fd845a0ef3daf1c4acfa4992
Alex Nita <67995057+alexnita3@users.noreply.github.com>
2026-09-02 00:06:38 +0100

    Added error page and check for whether it's in the correct place

--- src/main/java/com/galaxyrp/galaxyrplauncher/GalaxyRPLauncherApplication.java
+++ src/main/java/com/galaxyrp/galaxyrplauncher/GalaxyRPLauncherApplication.java
@@ -6,15 +6,30 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class GalaxyRPLauncherApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GalaxyRPLauncherApplication.class.getResource("galaxy.fxml"));
+        String screen;
+        if (isGameDataFolderPresent()) {
+            screen = "galaxy.fxml";
+        } else {
+            screen = "errorScreen.fxml";
+        }
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                GalaxyRPLauncherApplication.class.getResource(screen));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setResizable(false);
         stage.setTitle("GalaxyRP Launcher");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private boolean isGameDataFolderPresent() {
+        Path gameDataFolder = Paths.get("GameData");
+        return Files.isDirectory(gameDataFolder);
     }
 }
